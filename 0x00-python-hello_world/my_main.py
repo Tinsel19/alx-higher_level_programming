de96fdbdc5d2252cfc99006b894ac54efaa71481
Tinsel19 <olaoluwaoladunjoye130@gmail.com>
2023-03-06 08:04:05 -0800

    Tinsel19

--- 0x00-python-hello_world/my_main.py
+++ 0x00-python-hello_world/my_main.py
@@ -1,19 +1,1 @@
-#!/usr/bin/python3
-import os
-import py_compile
-import sys
 
-# Get the filename from the environment variable
-filename = os.environ.get('PYFILE')
-
-# Compile the file using py_compile
-py_compile.compile(filename)
-
-# Set the output filename with .pyc extension
-output_filename = filename + 'c'
-
-# Rename the compiled file to the output filename
-os.rename('__pycache__/' + filename + '.cpython-{}.pyc'.format(sys.version_info[0]), output_filename)
-
-# Remove the __pycache__ directory
-os.rmdir('__pycache__')
